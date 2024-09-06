f692d441042f78aa3017f7e36c67d8190160f99a
Cabezas-Juan-Manuel <juancabezas95.jc@gmail.com>
2024-09-06 20:10:33 -0300

    spring andando

---
+++ src/test/java/CodigoEnPantuflas/ServiciosYa/ServiciosYaApplicationTests.java
@@ -0,0 +1,13 @@
+package CodigoEnPantuflas.ServiciosYa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciosYaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
