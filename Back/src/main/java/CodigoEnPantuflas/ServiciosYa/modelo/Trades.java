1b5836ca3503fd2b2c4c50e66e9eed393e3ddb89
Cabezas-Juan-Manuel <juancabezas95.jc@gmail.com>
2024-09-17 18:23:16 -0300

    se crea rama para fix

--- Back/src/main/java/CodigoEnPantuflas/ServiciosYa/modelo/Trades.java
+++ Back/src/main/java/CodigoEnPantuflas/ServiciosYa/modelo/Trades.java
@@ -1,6 +1,5 @@
 package CodigoEnPantuflas.ServiciosYa.modelo;
 
-import jakarta.persistence.Entity;
 
 public enum Trades {
     PLUMBER, ELECTRICIAN, BRICK_LAYER, CARPENTER, LOCKSMITH, GAS_TECHNICIAN
