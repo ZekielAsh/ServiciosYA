aaafa2a1b17753be78ba5b6fc25e520d0b1d5212
MateoGiuffra <matteogiuffrah40@gmail.com>
2024-10-06 15:44:53 -0300

    endpoint sendRequesTo hecho como void y notfoundexception creada

--- Back/src/main/java/CodigoEnPantuflas/ServiciosYa/controller/rest/GlobalControllerAdviser.java
+++ Back/src/main/java/CodigoEnPantuflas/ServiciosYa/controller/rest/GlobalControllerAdviser.java
@@ -1,5 +1,6 @@
 package CodigoEnPantuflas.ServiciosYa.controller.rest;
 import CodigoEnPantuflas.ServiciosYa.controller.dto.ErrorDto;
+import CodigoEnPantuflas.ServiciosYa.exceptions.NotFound;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,6 +15,13 @@ public class GlobalControllerAdviser{
     public ResponseEntity<ErrorDto> handleGeneralException(Exception ex) {
         ErrorDto errorDto = new ErrorDto(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
         return ResponseEntity.internalServerError().body(errorDto);
+    }
+
+    @ExceptionHandler(NotFound.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ErrorDto> handleNotFoundException(Exception ex) {
+        ErrorDto errorDto = new ErrorDto(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDto);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
