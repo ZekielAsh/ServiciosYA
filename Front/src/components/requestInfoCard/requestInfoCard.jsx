3610a3561f0a05b45d7398a4bf332ad9f84ac067
MateoGiuffra <matteogiuffrah40@gmail.com>
2024-10-07 20:09:32 -0300

    card de solicitudes recibidas hecha

--- Front/src/components/requestInfoCard/requestInfoCard.jsx
+++ Front/src/components/requestInfoCard/requestInfoCard.jsx
@@ -3,17 +3,42 @@ import { Link } from "react-router-dom";
 import Button from "../button/Button";
 
 const RequestInfoCard = ({ request }) => {
-    console.log(request.user)
-  return (
-    <div className="request-info-card">
-      <div className="request-info-card-item">
-        <Link to={`/profile/${request.user.email}`}>{request.user.nickname}</Link>
+    console.log(request)
+    console.log("aca esta la desc: " + request.description)
+    return(
+      <div className="request-body">
+        <div className="request-info-title">
+        <strong> Nombre </strong> 
+          <Link to={`/profile/${request.user.email}`}>
+          {request.user.nickname}</Link>  
+        </div>
+        <strong> Descripcion </strong>
+           {request.description}
+        <div className="request-buttons">
+          <div className="request-button-left">
+            <Button>Aceptar</Button>
+          </div>
+          <div className="request-button-right">
+            <Button>Rechazar</Button>
+          </div>
+        </div>
       </div>
-      <div className="request-info-card-item">{request.title}</div>
-      <div className="request-info-card-item">{request.description}</div>
-      <div className="request-info-card-item"><Button>Aceptar</Button></div>
-      <div className="request-info-card-item"><Button>Rechazar</Button></div>
-    </div>
-  );
+    )    
 };
+
+
 export default RequestInfoCard;
+
+
+// return (
+//   <div className="request-info-">
+//       <Link to={`/profile/${request.user.email}`}>{request.user.nickname}</Link>
+//     <div className="request-info-title">{request.title}
+//       <div className="request-info-description">{request.description}</div>
+//     </div>
+//     <div className="request-button-column">
+//       <Button>Aceptar</Button>
+//       <Button>Rechazar</Button>
+//     </div>
+//   </div>
+// );
