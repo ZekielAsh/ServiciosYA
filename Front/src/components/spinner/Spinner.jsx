1c2467e88b32b5754beff8c7d4de96658e78d0a9
Matias Laime <matulai1221@gmail.com>
2024-10-13 01:06:04 -0300

    Se cambia fondo de spinner

--- Front/src/components/spinner/Spinner.jsx
+++ Front/src/components/spinner/Spinner.jsx
@@ -1,10 +1,14 @@
+import BackgroundSection from "../../components/backgroundSection/BackgroundSection.jsx";
 import "./Spinner.css";
 
 const Spinner = () => {
   return (
-    <div className="spinner-container">
-      <div className="spinner"></div>
-    </div>
+    <>
+      <BackgroundSection />
+      <div className="spinner-container">
+        <div className="spinner"></div>
+      </div>
+    </>
   );
 };
 
